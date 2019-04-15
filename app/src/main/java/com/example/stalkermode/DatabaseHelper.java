7cdc42193a4f54b14d25f493003b087c5d6e7eb5
Breandd <roy.vissers@hotmail.com>
2019-04-15 09:10:24 +0200

    test

---
+++ app/src/main/java/com/example/stalkermode/DatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.example.stalkermode;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "stalkermode.db";
+    public static final String TABLE_NAME = "bloedgroep";
+    public static final String ID = "ID";
+    public static final String BLOEGROEP = "Bloedgroep";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Bloedgroep TEXT) ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
