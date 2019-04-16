0a59aac6029ed788f15219c0b60591fd42b3e0ec
Breandd <roy.vissers@hotmail.com>
2019-04-16 14:11:30 +0200

    test

--- app/src/main/java/com/example/stalkermode/DatabaseHelper.java
+++ app/src/main/java/com/example/stalkermode/DatabaseHelper.java
@@ -7,7 +7,6 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "stalkermode.db";
-    public static final String TABLE_NAME = "bloedgroep";
     public static final String ID = "ID";
     public static final String BLOEGROEP = "Bloedgroep";
 
@@ -18,12 +17,47 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Bloedgroep TEXT) ");
+        db.execSQL("create table bloedgroep " + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Bloedgroep TEXT) ");
+        db.execSQL("create table land " + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Land TEXT) ");
+        db.execSQL("create table geslacht " + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Geslacht TEXT) ");
+        db.execSQL("create table seksuele_geaardheid " + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Geaardheid TEXT) ");
+        db.execSQL("create table lievelingskleur " + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Lievelingskleur TEXT) ");
+        db.execSQL("create table user " + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, Voornaam TEXT," +
+                " Tussenvoeglse TEXT," +
+                " Achternaam TEXT," +
+                " email TEXT," +
+                " Telefoonnummer TEXT," +
+                " Land INT," +
+                " Stad TEXT," +
+                " Straat TEXT," +
+                " Postcode TEXT," +
+                " Huisnummer INT," +
+                " Bloedgroep INT," +
+                " Lievelingskleur INT," +
+                " Geslacht INT," +
+                " Gehuwd BOOLEAN," +
+                " Kinderen INT," +
+                " Seksuele_geaardheid INT," +
+                " Brommerrijbewijs BOOLEAN," +
+                " Motorrijwijs BOOLEAN," +
+                " BSNNummer TEXT," +
+                " Lengte FLOAT," +
+                " Gewicht FLOAT," +
+                " Facebook TEXT," +
+                " Twitter TEXT," +
+                " Discord TEXT," +
+                " Snapchat TEXT," +
+                " Instagram TEXT," +
+                " Linkedin TEXT," +
+                "FOREIGN KEY (Land) REFERENCES land(ID)," +
+                "FOREIGN KEY (Bloedgroep) REFERENCES bloedgroep(ID)," +
+                "FOREIGN KEY (Lievelingskleur) REFERENCES lievelingskleur(ID)," +
+                "FOREIGN KEY (Geslacht) REFERENCES geslacht(id)," +
+                "FOREIGN KEY (Seksuele_geaardheid) REFERENCES seksuele_geaardheid(ID)) ");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if EXISTS " + TABLE_NAME);
         onCreate(db);
     }
 }
--- app/src/main/java/com/example/stalkermode/MainActivity.java
+++ app/src/main/java/com/example/stalkermode/MainActivity.java
@@ -1,15 +1,116 @@
 package com.example.stalkermode;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
+
+    private int CONTACT_PERMISSION_CODE = 1;
+
+    private Button loadContacts;
+    private TextView listContacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDb = new DatabaseHelper(this);
+        listContacts = (TextView) findViewById(R.id.listContacts);
+        loadContacts = (Button) findViewById(R.id.loadContacts);
+        loadContacts.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
+                    loadContacts();
+                } else{
+                    requestPermissions();
+                }
+            }
+        });
+    }
+
+    private void loadContacts(){
+        StringBuilder builder = new StringBuilder();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+        if (cursor.getCount() > 0) {
+            while(cursor.moveToNext()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+
+                if (hasPhoneNumber > 0) {
+                    Cursor cursor2 = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+
+                    while (cursor2.moveToNext()) {
+                        String phoneNumber = cursor2.getString(cursor2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        builder.append("Contact : ").append(name).append("\n\n").append("Phone Number :").append(phoneNumber).append("\n\n");
+                    }
+
+                    cursor2.close();
+                }
+            }
+        }
+        cursor.close();
+        listContacts.setText(builder.toString());
+    }
+    private void requestPermissions() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)){
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed to load in existing contacts")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener(){
+                        @Override
+                        public void onClick(DialogInterface dialog, int which){
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_CONTACTS}, CONTACT_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+
+
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_CONTACTS}, CONTACT_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CONTACT_PERMISSION_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denies", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
+
